873fced36ccae2af7863218d9cc380bc5d681c10
kiritoMME <77681921+kiritoMME@users.noreply.github.com>
2023-06-19 16:08:23 +0300

    edit command in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 
 echo "BUILD START"
+python3.9 -m pip install django
+python3.9 -m pip install wheel
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
